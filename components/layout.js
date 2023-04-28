e7459c7754dda389ffbb60aacefc9ed66d3a775c
Kyle <kbburkholder@gmail.com>
2023-04-28 18:27:50 -0400

    changed site title

--- components/layout.js
+++ components/layout.js
@@ -5,7 +5,7 @@ import utilStyles from '../styles/utils.module.css';
 import Link from 'next/link';
 
 const name = 'Kyle Burkholder';
-export const siteTitle = 'Next.js Sample Website';
+export const siteTitle = 'Kyle Burkholder';
 
 export default function Layout({ children, home }) {
   return (
